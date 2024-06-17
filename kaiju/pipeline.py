7a45881f28915ca8234eb89f00809a53ee83f893
ninja-96 <okachalov@icloud.com>
2024-06-17 11:32:42 +0000

    add advanced runner support

--- kaiju/pipeline.py
+++ kaiju/pipeline.py
@@ -2,7 +2,8 @@ import asyncio
 from typing import List
 
 from kaiju.item import BaseItem
-from kaiju.runner import Runner, AsyncRunner
+from kaiju.runner import Runner, AsyncRunner, AdvancedRunner
+from kaiju.runner.advanced_runner import advanced_runner_call
 
 
 __all__ = [
@@ -17,12 +18,14 @@ class Pipeline:
                 '\'runners\' must be list of \'Runner\'s'
             )
 
-        if not all(isinstance(r, (Runner, AsyncRunner)) for r in runners):
+        if not all(isinstance(r, (Runner, AsyncRunner, AdvancedRunner)) for r in runners):
             raise TypeError(
                 'all \'runners\' must be list of \'Runner\'s'
             )
 
         self._runners = runners
+        for runner in self._runners:
+            runner.start()
 
     async def __call__(self, item: BaseItem) -> BaseItem:
         if not isinstance(item, BaseItem):
@@ -33,10 +36,22 @@ class Pipeline:
         for runner in self._runners:
             if isinstance(runner, Runner):
                 item = await loop.run_in_executor(
-                    runner.pool, runner, item
+                    runner.pool, runner.run, item
                 )
 
             if isinstance(runner, AsyncRunner):
-                item = await runner(item)
+                item = await runner.async_run(item)
+
+            if isinstance(runner, AdvancedRunner):
+                item = await loop.run_in_executor(
+                    runner.pool,
+                    advanced_runner_call,
+                    runner.queue,
+                    item
+                )
 
         return item
+
+    def __del__(self) -> None:
+        for runner in self._runners:
+            runner.stop()
